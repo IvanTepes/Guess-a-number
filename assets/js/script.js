6faaab0714f7518924bfc84e56e6bcc207a0f3ce
Ivan Tepes <Ivan Tepes@DESKTOP-CKMILME>
2022-10-06 22:20:20 +0100

    Dry out code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,35 @@
 ('use strict');
 
-// ! Game Logic and Messages
+// ! Initial setup
 
+// Generate random number
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
+// Initial score
 let score = 20;
 
-// document.querySelector('.secret-number__col--number').textContent =
-//     secretNumber;
+// Return dom element
+const getElement = function (className) {
+    return document.querySelector(className);
+};
 
-document.querySelector(
-    '.check__result--score'
-).textContent = `Score : ${score}`;
+// An Object with html elements
+const renderMessageTo = {
+    header: getElement('.header__title'),
+    instruction: getElement('.instruction__title'),
+    number: getElement('.secret-number__col--number'),
+    message: getElement('.message__title'),
+    numberEl: getElement('.secret-number__col'),
+    score: getElement('.check__result--score'),
+};
+console.log(renderMessageTo);
 
-document.getElementById('js-message').textContent = 'Start Guessing ..';
+// Render initial score, message and instruction
+renderMessageTo.score.textContent = `Score : ${score}`;
 
-document.getElementById('js-instruction__title').textContent = 'Between 1 - 20';
+renderMessageTo.message.textContent = 'Start Guessing ..';
+
+renderMessageTo.instruction.textContent = 'Between 1 - 20';
 
 document
     .querySelector('.check__input--number-btn')
@@ -23,58 +37,52 @@ document
         const guess = Number(
             document.querySelector('.check__input--number').value
         );
-        let header = document.getElementById('js-header__title');
-        let message = document.getElementById('js-message');
-        let instruction = document.getElementById('js-instruction__title');
-        let number = document.getElementById('js-secret-number');
-        let numberEl = document.querySelector('.secret-number__col');
-        console.log(numberEl);
 
         if (!guess) {
-            message.textContent = 'Please Select a Number!';
+            renderMessageTo.message.textContent = 'Please select a Number!';
         } else if (guess === secretNumber) {
-            header.textContent = 'Congrats';
-            instruction.textContent = 'Number';
-            number.textContent = `${secretNumber}`;
-            message.textContent = 'is Correct!';
+            renderMessageTo.header.textContent = 'Congrats';
+            renderMessageTo.instruction.textContent = 'Number';
+            renderMessageTo.number.textContent = `${secretNumber}`;
+            renderMessageTo.message.textContent = 'is Correct!';
         } else if (guess > secretNumber) {
-            header.textContent = 'Sorry';
-            instruction.textContent = 'Number';
-            number.textContent = `is`; //`${guess}`
-            message.textContent = 'is Too High';
+            renderMessageTo.header.textContent = 'Sorry';
+            renderMessageTo.instruction.textContent = 'Number';
+            renderMessageTo.number.textContent = `${guess}`;
+            renderMessageTo.message.textContent = 'is Too High!';
             score--;
-            document.querySelector(
-                '.check__result--score'
-            ).textContent = `Score : ${score}`;
+            renderMessageTo.score.textContent = `Score : ${score}`;
         } else if (guess < secretNumber) {
-            header.textContent = 'Sorry';
-            instruction.textContent = 'Number';
-            number.textContent = `${guess}`;
-            message.textContent = ' is Too Low';
+            renderMessageTo.header.textContent = 'Sorry';
+            renderMessageTo.instruction.textContent = 'Number';
+            renderMessageTo.number.textContent = `${guess}`;
+            renderMessageTo.message.textContent = 'is Too Low!';
             score--;
-            document.querySelector(
-                '.check__result--score'
-            ).textContent = `Score : ${score}`;
+            renderMessageTo.score.textContent = `Score : ${score}`;
         }
 
         if (guess === secretNumber) {
-            numberEl.classList.add('number__correct');
+            renderMessageTo.numberEl.classList.add('number__correct');
             if (
-                numberEl.classList.contains('number__to-high') ||
-                numberEl.classList.contains('number__to-low')
+                renderMessageTo.numberEl.classList.contains(
+                    'number__to-high'
+                ) ||
+                renderMessageTo.numberEl.classList.contains('number__to-low')
             ) {
-                numberEl.classList.remove('number__to-high');
-                numberEl.classList.remove('number__to-low');
+                renderMessageTo.numberEl.classList.remove('number__to-high');
+                renderMessageTo.numberEl.classList.remove('number__to-low');
             }
         } else if (guess > secretNumber) {
-            numberEl.classList.add('number__to-high');
-            if (numberEl.classList.contains('number__to-low')) {
-                numberEl.classList.remove('number__to-low');
+            renderMessageTo.numberEl.classList.add('number__to-high');
+            if (renderMessageTo.numberEl.classList.contains('number__to-low')) {
+                renderMessageTo.numberEl.classList.remove('number__to-low');
             }
         } else if (guess < secretNumber) {
-            numberEl.classList.add('number__to-low');
-            if (numberEl.classList.contains('number__to-high')) {
-                numberEl.classList.remove('number__to-high');
+            renderMessageTo.numberEl.classList.add('number__to-low');
+            if (
+                renderMessageTo.numberEl.classList.contains('number__to-high')
+            ) {
+                renderMessageTo.numberEl.classList.remove('number__to-high');
             }
         }
     });
